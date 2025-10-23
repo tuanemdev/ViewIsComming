d765698b3554c6fa32d1bc82a0e4d87663eec9ed
Nguyen Tuan Anh <nguyentuananh.developer@gmail.com>
2025-10-23 18:02:04 +0700

    update platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "ViewIsComming",
+    platforms: [.iOS(.v17), .visionOS(.v2), .macOS(.v14), .watchOS(.v10)],
     products: [
         .library(
             name: "ViewIsComming",
